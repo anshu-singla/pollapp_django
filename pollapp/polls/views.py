48f3d09175e4182028c928fa11d3e57b0955673a
Anshu <anshusingla10121996@gmail.com>
2016-12-18 17:10:21 +0530

    Adding some new views into the views.py

--- pollapp/polls/views.py
+++ pollapp/polls/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello,world.You're at the polls index.")
+
+def detail(request,question_id):
+	return HttpResponse("Hi,You are looking at question %s." % question_id)
+
+def result(request,question_id):
+	response = "You're looking at result of question %s ."
+	return HttpResponse(response % question_id)
+
+def vote(reuest,question_id):
+	return HttpResponse("You're are voting on question %s." % question_id)
+
